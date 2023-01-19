bb80b7e6f3f6434247f3003d0cd7339e017d7f1f
Patryk Laurent <patryk@pakl.net>
2023-01-18 19:20:16 -0800

    personal font size adjustments

--- apps/mtxcli/src/mtxcli.rs
+++ apps/mtxcli/src/mtxcli.rs
@@ -68,7 +68,7 @@ impl Mtxcli{
             content,
             gam,
             screensize,
-            bubble_width: ((screensize.x / 5) * 4) as u16, // 80% width for the text bubbles
+            bubble_width: ((screensize.x / 10) * 9) as u16, // 90% (was 80%) width for the text bubbles
             margin: Point::new(4, 4),
             bubble_margin: Point::new(4, 4),
             bubble_radius: 4,
@@ -190,7 +190,7 @@ impl Mtxcli{
             bubble_tv.draw_border = true;
             bubble_tv.clear_area = true;
             bubble_tv.rounded_border = Some(self.bubble_radius);
-            bubble_tv.style = GlyphStyle::Regular;
+            bubble_tv.style = GlyphStyle::Large;
             bubble_tv.margin = self.bubble_margin;
             bubble_tv.ellipsis = false; bubble_tv.insertion = None;
             write!(bubble_tv.text, "{}", h.text.as_str()).expect("couldn't write history text to TextView");
