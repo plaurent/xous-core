d0395af972e6307df4aa58626616a761985ad30d
Patryk Laurent <patryk.laurent@scenera.net>
2023-07-07 17:09:40 -0700

    add a few commands

--- apps/edlin/src/cmds.rs
+++ apps/edlin/src/cmds.rs
@@ -80,11 +80,97 @@ impl CommonEnv {
 ///// 1. add your module here, and pull its namespace into the local crate
 mod audio;     use audio::*;
 
+
+enum EdlinMode {
+    Inserting,
+    Command
+}
+
+pub struct Edlin {
+    data:Vec<std::string::String>,
+    //data:Vec<String<512>>,
+    mode:EdlinMode,
+    line_cursor: usize
+}
+
+impl Edlin {
+    pub fn process(&mut self, line:&std::string::String) -> Vec<std::string::String> {
+
+        match self.mode {
+            EdlinMode::Inserting => {
+                if line.trim().eq(".") {
+                    self.mode = EdlinMode::Command;
+                    return Vec::new();
+                } else {
+                    self.data.insert(self.line_cursor, std::string::String::from(line));  // TODO Insert line at current line_cursor
+                    self.line_cursor += 1;
+                }
+            }
+            EdlinMode::Command => {
+                if line.to_lowercase().starts_with("i") || line.to_lowercase().ends_with("i") {
+                    self.mode = EdlinMode::Inserting;
+                    if !line.to_lowercase().starts_with("i") {
+                        let digits: Vec<&str> = line.matches(char::is_numeric).collect();
+                        let mut line_to_insert_before = digits.join("").parse::<usize>().unwrap();
+                        if line_to_insert_before >= self.data.len() {
+                            line_to_insert_before = self.data.len()
+                        }
+                        self.line_cursor = line_to_insert_before - 1
+                    }
+                }
+                if line.to_lowercase().ends_with("d") {
+                    let mut del_start = self.line_cursor-1;
+                    let mut del_cease = self.line_cursor;
+                    let mut without_d = line.to_lowercase().replace("d", "");
+                    if without_d.contains(",") {
+                        let pair: Vec<&str> = without_d.split(',').collect();
+                        del_start = pair[0].parse::<usize>().unwrap();
+                        del_cease = pair[1].parse::<usize>().unwrap() + 1;
+                    } else if without_d.len() > 0 {
+                        del_start = without_d.parse::<usize>().unwrap();
+                        del_cease = without_d.parse::<usize>().unwrap() + 1;
+                    }
+                    if del_cease > self.data.len() {
+                        del_cease = self.data.len();
+                    }
+                    if del_start >= del_cease {
+                        del_start = del_cease - 1;
+                    }
+                    println!("Deleting {} to {}", del_start, del_cease);
+                    for i in (del_start..del_cease).rev() {
+                        self.data.remove(i);
+                        if self.line_cursor > self.data.len() {
+                            self.line_cursor = self.data.len()
+                        }
+                    }
+
+                }
+                if line.contains("p") || line.contains("P") {
+                    return self.data.clone()
+                }
+                if line.contains("l") || line.contains("L") {
+                    let mut result: Vec<std::string::String> = Vec::new();
+                    for (i, line) in self.data.iter().enumerate() {
+                        result.insert(i, format!("{}: {}", i, line));
+                    }
+                    return result;
+                }
+            }
+        }
+        return Vec::new();
+    }
+}
+
+
+
+
+
 pub struct CmdEnv {
     common_env: CommonEnv,
     lastverb: String::<256>,
     ///// 2. declare storage for your command here.
     audio_cmd: Audio,
+    edlin: Edlin,
 }
 impl CmdEnv {
     pub fn new(xns: &xous_names::XousNames) -> CmdEnv {
@@ -100,12 +186,25 @@ impl CmdEnv {
             trng: Trng::new(&xns).unwrap(),
             xns: xous_names::XousNames::new().unwrap(),
         };
+
+        let mut edlin = Edlin {
+            data: Vec::new(),
+            mode: EdlinMode::Command,
+            line_cursor: 0
+        };
+        edlin.data.push(std::string::String::from("Hello world."));
+        edlin.data.push(std::string::String::from("This is a test."));
+        edlin.line_cursor = 2;
+
+
+
         log::info!("done creating CommonEnv");
         CmdEnv {
             common_env: common,
             lastverb: String::<256>::new(),
             ///// 3. initialize your storage, by calling new()
             audio_cmd: Audio::new(&xns),
+            edlin: edlin,
         }
     }
 
@@ -118,42 +217,62 @@ impl CmdEnv {
         ];
 
         if let Some(cmdline) = maybe_cmdline {
-            let maybe_verb = tokenize(cmdline);
 
-            let mut cmd_ret: Result<Option<String::<1024>>, xous::Error> = Ok(None);
-            if let Some(verb_string) = maybe_verb {
-                let verb = verb_string.to_str();
-
-                // search through the list of commands linearly until one matches,
-                // then run it.
-                let mut match_found = false;
-                for cmd in commands.iter_mut() {
-                    if cmd.matches(verb) {
-                        match_found = true;
-                        cmd_ret = cmd.process(*cmdline, &mut self.common_env);
-                        self.lastverb.clear();
-                        write!(self.lastverb, "{}", verb).expect("couldn't record last verb");
-                    };
+            match self.edlin.mode {
+                EdlinMode::Command => {
+                    write!(ret, "* ");
                 }
-
-                // if none match, create a list of available commands
-                if !match_found {
-                    let mut first = true;
-                    write!(ret, "Commands: ").unwrap();
-                    for cmd in commands.iter() {
-                        if !first {
-                            ret.append(", ")?;
-                        }
-                        ret.append(cmd.verb())?;
-                        first = false;
-                    }
-                    Ok(Some(ret))
-                } else {
-                    cmd_ret
+                EdlinMode::Inserting => {
+                    write!(ret, " {}: ", self.edlin.line_cursor);
                 }
-            } else {
-                Ok(None)
             }
+            let line = std::string::String::from(cmdline.as_str().unwrap());
+            let result = self.edlin.process(&line);
+            //let result = self.edlin.process(&std::string::String::from(line.trim()));
+            for result_line in result {
+                write!(ret, "{}\n", result_line);
+            }
+
+
+            Ok(Some(ret))
+
+
+            //let maybe_verb = tokenize(cmdline);
+
+            //let mut cmd_ret: Result<Option<String::<1024>>, xous::Error> = Ok(None);
+            //if let Some(verb_string) = maybe_verb {
+            //    let verb = verb_string.to_str();
+
+            //    // search through the list of commands linearly until one matches,
+            //    // then run it.
+            //    let mut match_found = false;
+            //    for cmd in commands.iter_mut() {
+            //        if cmd.matches(verb) {
+            //            match_found = true;
+            //            cmd_ret = cmd.process(*cmdline, &mut self.common_env);
+            //            self.lastverb.clear();
+            //            write!(self.lastverb, "{}", verb).expect("couldn't record last verb");
+            //        };
+            //    }
+
+            //    // if none match, create a list of available commands
+            //    if !match_found {
+            //        let mut first = true;
+            //        write!(ret, "Commands: ").unwrap();
+            //        for cmd in commands.iter() {
+            //            if !first {
+            //                ret.append(", ")?;
+            //            }
+            //            ret.append(cmd.verb())?;
+            //            first = false;
+            //        }
+            //        Ok(Some(ret))
+            //    } else {
+            //        cmd_ret
+            //    }
+            //} else {
+            //    Ok(None)
+            //}
         } else if let Some(callback) = maybe_callback {
             let mut cmd_ret: Result<Option<String::<1024>>, xous::Error> = Ok(None);
             // first check and see if we have a callback registration; if not, just map to the last verb
